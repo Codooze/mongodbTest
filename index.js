b1a2ee7281d45a24c28fa712ba2dd9778bbf839d
Jeison <jeysonm021@gmail.com>
2023-03-06 09:49:15 -0500

    adding html file presentation

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ import { connection } from "./db.js";
 import carRoutes from "./api/car.route.js";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { join } from "path";
 
 dotenv.config();
 
 const app = express();
+
+// Obtener la ruta del archivo HTML
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, "..");
+
 app.use(bodyParser.json());
 app.use(cors());
 
 connection();
 app.use("/api/car", carRoutes);
+
+// Ruta para enviar el archivo HTML
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
